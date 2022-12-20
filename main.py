443155d59c96b934ac196a730cd960b2a1f77a62
Denis Kel <asket2013@ya.ru>
2022-12-20 23:20:35 +1000

    Исправлено имя функции

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from pprint import pprint
 import csv
 
 
-def convect_book(pattern_raw, pattern_new, testbook):
+def corrected_book(pattern_raw, pattern_new, testbook):
     correct_list = list()
     for line in testbook:
         line_to_string = ','.join(line)
@@ -25,13 +25,13 @@ def inspect_phonebook(phonebook):
     # task 1
     name_pattern_raw = r'^([А-ЯЁа-яё]+)(\s*)(\,?)([А-ЯЁа-яё]+)(\s*)(\,?)([А-ЯЁа-яё]*)(\,?)(\,?)(\,?)'
     name_pattern_new = r'\1\3\10\4\6\9\7\8'
-    contacts_correct_1 = convect_book(name_pattern_raw, name_pattern_new, contacts_list)
+    contacts_correct_1 = corrected_book(name_pattern_raw, name_pattern_new, contacts_list)
 
     # task 2
     number_pattern_raw = r'(\+7|8)(\s*)(\(*)(\d{3})(\)*)(\s*)(\-*)(\d{3})(\s*)(\-*)(\d{2})(\s*)(\-*)(\d{2})(\s*)' \
                          r'(\(*)(доб)*(\.*)(\s*)(\d+)*(\)*)'
     number_pattern_new = r'+7(\4)\8-\11-\14\15\17\18\19\20'
-    contacts_correct_2 = convect_book(number_pattern_raw, number_pattern_new, contacts_correct_1)
+    contacts_correct_2 = corrected_book(number_pattern_raw, number_pattern_new, contacts_correct_1)
 
     # task 3
     for i in contacts_correct_2:
